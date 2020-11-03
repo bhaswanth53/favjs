8ad0e6469a1903b8cfa37e888b547ea62e32b73b
Bhaswanth53 <bhaswanth22@gmail.com>
2020-11-03 20:42:05 +0530

    SOlved loader issue

--- index.js
+++ index.js
@@ -1,23 +1,27 @@
-export default class Fav {
-    data = {}
-    errors = {}
-    elem = ""
-    patterns = {
-        url: /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/,
-        email: /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i,
-        password: /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/
-    }
+export default class FAV {
     constructor(data) {
         this.data = data
+        this.errors = {}
+        this.elem = ""
+        this.id = ""
+        this.patterns = {
+            url: /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/,
+            email: /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i,
+            password: /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/
+        }
     }
     value(elem) {
         this.elem = elem
         return this
     }
+    name(elem) {
+        this.id = elem
+        return this
+    }
     required() {
         var value = this.data[this.elem]
         if(value == "" || value.length == 0 || value == undefined) {
-            this.error(this.elem, "This field is required")
+            this.error(this.elem, this.id+" is required")
         }
         return this
     }
@@ -29,28 +33,28 @@ export default class Fav {
     minValue(size) {
         var value = this.data[this.elem]
         if(value < size) {
-            this.error(this.elem, "Minimum value is "+size)
+            this.error(this.elem, this.id+" minimum value is "+size)
         }
         return this
     }
     maxValue(size) {
         var value = this.data[this.elem]
         if(value > size) {
-            this.error(this.elem, "Maximum value is "+size)
+            this.error(this.elem, this.id+" maximum value is "+size)
         }
         return this
     }
     minLength(size) {
         var value = this.data[this.elem]
         if(value.length < size) {
-            this.error(this.elem, "Atleast "+ size +" characters are required")
+            this.error(this.elem, this.id+" atleast "+ size +" characters are required")
         }
         return this
     }
     maxLength(size) {
         var value = this.data[this.elem]
         if(value.length > size) {
-            this.error(this.elem, "Maximum "+ size + " characters are allowed")
+            this.error(this.elem, this.id+" maximum "+ size + " characters are allowed")
         }
         return this
     }
@@ -58,7 +62,7 @@ export default class Fav {
         var value = this.data[this.elem]
         var regex = new RegExp(this.patterns.url)
         if(!regex.test(value)) {
-            this.error(this.elem, "Value must be an URL")
+            this.error(this.elem, this.id+" value must be an URL")
         }
         return this
     }
@@ -66,7 +70,7 @@ export default class Fav {
         var value = this.data[this.elem]
         var regex = new RegExp(this.patterns.email)
         if(!regex.test(value)) {
-            this.error(this.elem, "Email is not valid")
+            this.error(this.elem, this.id+" is not a valid email")
         }
         return this
     }
@@ -74,7 +78,7 @@ export default class Fav {
         var value = this.data[this.elem]
         var regex = new RegExp(this.patterns.password)
         if(!regex.test(value)) {
-            this.error(this.elem, "Password must contain one uppercase letter, one number, one special character and one letter and atleast eight characters length")
+            this.error(this.elem, this.id+" must contain one uppercase letter, one number, one special character and one letter and atleast eight characters length")
         }
         return this
     }
@@ -83,14 +87,14 @@ export default class Fav {
         var regex = new RegExp(pat)
         if(!regex.test(value))
         {
-            this.error(this.elem, "Value not matched to pattern")
+            this.error(this.elem, this.id+" not matched to pattern")
         }
         return this
     }
     equals(val) {
         var value = this.data[this.elem]
         if(value !== val) {
-            this.error(this.elem, "Values not matched")
+            this.error(this.elem, this.id+" not matched")
         }
         return this
     }
@@ -98,7 +102,10 @@ export default class Fav {
         if(this.errors[elem] == "" || this.errors[elem] == undefined)
             this.errors[elem] = value
     }
-    geterrors() {
+    getErrors() {
         return this.errors
+    }
+    isSuccess() {
+        return Object.keys(this.errors).length == 0
     }
 }
